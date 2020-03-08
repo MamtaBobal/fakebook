ffcabdad24324bcee4b358a7a505586383adbae0
Mamta Bhardwaj <mamta.bhardwaj@builder.ai>
2020-03-08 13:44:31 +0530

    Add methods to Comments Controller

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,7 +1,36 @@
 class CommentsController < ApplicationController
+	include ApplicationHelper
+
   def new
+		@comment = Comment.new
   end
 
   def create
+		@comment = current_user.comments.build(comments_params)
+		@post = Post.find(params[:comment][:post_id])
+		if @comment.save!
+			@notification = new_notification(@post.user, @post.id,
+			 'comment')
+			@notification.save!
+		end
+		redirect_to @post
+  end
+
+  def destroy
+		@comment = Comment.find(params[:id])
+		@post = @comment.post
+		# User can destroy his/her own comments
+		return unless current_user.id == @comment.user_id
+		if @comment.destroy
+			flash[:success] = 'Comment deleted'
+			redirect_to @post
+		else
+			redirect_to @comment
+		end
+  end
+
+  private
+  def comments_params
+		params.require(:comment).permit(:content, :post_id)
   end
 end
