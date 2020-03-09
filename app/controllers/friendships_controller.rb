d653593dcd7c5f43c5f190b3e178e342983f4b7f
Mamta Bhardwaj <mamta.bhardwaj@builder.ai>
2020-03-09 17:12:02 +0530

    Add methods to Friendships controller

--- app/controllers/friendships_controller.rb
+++ app/controllers/friendships_controller.rb
@@ -1,4 +1,45 @@
 class FriendshipsController < ApplicationController
+	include ApplicationHelper
+
   def create
+		# Disallow the ability to send yourself a friend request
+		return if current_user.id == params[:user_id]
+		# Disallow the ability to send friend request more than once to same person
+		return if friend_request_sent?(User.find(params[:user_id]))
+		# Disallow the ability to receive friend request from someone who already sent you one
+		return if friend_request_received?(User.find(params[:user_id]))
+
+		@user = User.find(params[:user_id])
+		@friendship = current_user.friend_sent.build(sent_to_id: params[:user_id], status: false)
+		if @friendship.save
+			flash[:success] = 'Friend Request Sent!'
+			@notification = new_notification(@user, @current_user.id, 'friendRequest')
+			@notification.save
+		else
+			flash[:danger] = 'Friend Request Failed!'
+		end
+		redirect_back(fallback_location: root_path)
+  end
+
+  def accept_friend
+		@friendship = Friendship.find_by(sent_to_id: current_user.id, sent_by_id: params[:user_id], status: false)
+		return unless @friendship
+		@friendship.status = true
+		if @friendship.save
+			flash[:success] = 'Friend Request Accepted!'
+			@friendship2 = current_user.friend_sent.build(sent_to_id: params[:user_id], status: true)
+			@friendship2.save
+		else
+			flash[:danger] = 'Friend Request could not be accepted'
+		end
+		redirect_back(fallback_location: root_path)
+  end
+
+  def decline_friend
+		@friendship = Friendship.find_by(sent_to_id: current_user.id, sent_by_id: params[:user_id], status: false)
+		return unless @friendship
+		@friendship.destroy
+		flash[:success] = 'Friend Request Declined!'
+		redirect_back(fallback_location: root_path)
   end
 end
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -10,4 +10,31 @@ module ApplicationHelper
 	def notification_find(notice)
 		# Pending
 	end
+
+	# Checks whether a post or a comment has already been liked by the
+	 # current user returning either true or false
+		def liked?(subject, type)
+			result = false
+			result = Like.where(user_id: current_user.id, post_id: subject.id).exists? if type == 'post'
+			result = Like.where(user_id: current_user.id, comment_id: subject.id).exists? if type == 'comment'
+			result
+		end
+
+	def friend_request_sent?(user)
+		current_user.friend_sent.exists?(sent_to_id: user.id, status: false)
+	end
+
+	def friend_request_received?(user)
+		current_user.friend_request.exists?(sent_by_id: user.id, status: false)
+	end
+
+	def possible_friend?(user)
+		request_sent = current_user.friend_sent.exists?(sent_to_id: user.id)
+		request_received = current_user.friend_request.exists?(sent_by_id: user.id)
+
+		return true if request_sent != request_received
+		return true if request_sent == request_received if request_sent == true
+		return false if request_sent == request_received if request_sent == false
+	end
+
 end
